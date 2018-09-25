cc6ee097475e98781625092ec54fbd56add75037
Shibobrota <shibobrota@gmail.com>
2018-09-25 19:40:52 +0530

    SQS - Dynamically Created & Assigned Policy(Send & Receive)

--- services/utility/handler.js
+++ services/utility/handler.js
@@ -1,5 +1,5 @@
 exports.handler = (event, context, callback) => {
     console.log("event ",event);
     console.log("environmentVariables",{ handler: process.env['handler']+"", processId: process.env['processId']+"", tableName: process.env['tableName']+"" });
-  callback(null, {message:"Successfully Invoked!", environmentVariables:{handler:process.env['handler'], processId:process.env['processId'], tableName: process.env['apiPrefix']+"",queue1:process.env['queue1']+""}});
+  callback(null, {event:event, context:context, message:"Successfully Invoked!", environmentVariables:{handler:process.env['handler'], processId:process.env['processId'], tableName: process.env['apiPrefix']+"",p1:process.env['p1']+"",p2:process.env['p2']+"",p3:process.env['p3']+""}});
 };
